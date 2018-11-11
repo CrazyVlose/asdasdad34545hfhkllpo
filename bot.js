764ca45b1780b2093b81a049864283bc562effe3
CrazyVlose <44671606+CrazyVlose@users.noreply.github.com>
2018-11-11 16:03:18 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+﻿
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const adminprefix = "-";
+const developers = ['451129303137255434'];
+
+console.log("3thhh2bbb.");
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`Changing The Playing To : **${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`Changing The Watching To : **${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`Changing The Listening To : **${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/inikolaiii");
+      message.channel.send(`Changing The Streaming To : **${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'sn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}**`)
+} else
+if (message.content.startsWith(adminprefix + 'sa')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To : **${argresult}**`);
+}
+});
+
+client.login(process.env.BOT_TOKEN);
